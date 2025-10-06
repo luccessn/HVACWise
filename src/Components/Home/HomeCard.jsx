8122a8c85e8f07ff9ffc2c3cae757e065ffef5c3
luccessn <gulualuka0@gmail.com>
2025-10-05 22:47:20 -0700

    dad

--- src/Components/Home/HomeCard.jsx
+++ src/Components/Home/HomeCard.jsx
@@ -52,6 +52,7 @@ export const HomeCard = () => {
     });
   };
   // const panelBackground = "#e6f0ff";
+  const [clear, setclear] = useState(false);
   return (
     <div className="p-6  mx-auto font-sans ">
       <div className="flex justify-between">
@@ -91,7 +92,7 @@ export const HomeCard = () => {
           <input className="file" name="text" type="file" />
         </button>
         <button
-          onClick={() => dispatch(clearFullHVAC())}
+          onClick={() => setclear(true)}
           className="inline-flex  items-center px-4 py-2 bg-red-600 transition ease-in-out duration-300 hover:bg-red-700 text-white text-sm font-medium rounded-md hover:-translate-y-1 hover:scale-110"
         >
           <svg
@@ -110,44 +111,56 @@ export const HomeCard = () => {
           </svg>
           გასუფთავება მთლიანად
         </button>
-        {/* <div
-  className="group select-none w-[250px] flex flex-col p-4 relative items-center justify-center bg-gray-800 border border-gray-800 shadow-lg rounded-2xl"
->
-  <div>
-    <div className="text-center p-3 flex-auto justify-center">
-      <svg
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        className="group-hover:animate-bounce w-12 h-12 flex items-center text-gray-600 fill-red-500 mx-auto"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          clipRule="evenodd"
-          fillRule="evenodd"
-          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-        ></path>
-      </svg>
-      <h2 className="text-xl font-bold py-4 text-gray-200">Are you sure?</h2>
-      <p className="font-bold text-sm text-gray-500 px-2">
-        Do you really want to continue? This process cannot be undone.
-      </p>
-    </div>
-    <div className="p-2 mt-2 text-center space-x-1 md:block">
-      <button
-        className="mb-2 md:mb-0 bg-gray-700 px-5 py-2 text-sm shadow-sm font-medium tracking-wider border-2 border-gray-600 hover:border-gray-700 text-gray-300 rounded-full hover:shadow-lg hover:bg-gray-800 transition ease-in duration-300"
-      >
-        Cancel
-      </button>
-      <button
-        className="bg-red-500 hover:bg-transparent px-5 ml-4 py-2 text-sm shadow-sm hover:shadow-lg font-medium tracking-wider border-2 border-red-500 hover:border-red-500 text-white hover:text-red-500 rounded-full transition ease-in duration-300"
-      >
-        Confirm
-      </button>
-    </div>
-  </div>
-</div> */}
       </div>
       <AddFloor />
+      {clear && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+          <div className="group select-none w-[320px] flex flex-col p-6 items-center justify-center bg-gray-800 border border-gray-700 shadow-xl rounded-2xl">
+            <div className="text-center p-3 flex-auto justify-center">
+              <svg
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                className="group-hover:animate-bounce w-12 h-12 flex items-center text-gray-600 fill-red-500 mx-auto"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 
+            002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 
+            1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 
+            0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 
+            0V8a1 1 0 00-1-1z"
+                />
+              </svg>
+              <h2 className="text-xl font-bold py-4 text-gray-200">
+                დარწნუნებული ხარ?
+              </h2>
+              <p className="font-bold text-sm text-gray-400 px-2">
+                დაიმახსოვრე გასუფთავებული ინფორმაცია ვეღარ აღდგება.
+              </p>
+            </div>
+            <div className="p-2 mt-4 text-center space-x-2 md:block">
+              <button
+                onClick={() => setclear(false)}
+                className="mb-2 md:mb-0 bg-gray-700 px-5 py-2 text-sm shadow-sm font-medium tracking-wider border border-gray-600 hover:border-gray-500 text-gray-300 rounded-full hover:shadow-lg hover:bg-gray-800 transition ease-in duration-200"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={() => {
+                  dispatch(clearFullHVAC());
+                  setclear(false);
+                }}
+                className="bg-red-500 hover:bg-transparent px-5 ml-2 py-2 text-sm shadow-sm hover:shadow-lg font-medium tracking-wider border border-red-500 hover:border-red-500 text-white hover:text-red-500 rounded-full transition ease-in duration-200"
+              >
+                Confirm
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
       <div className="flex justify-center  ">
         <div
           className={`overflow-y-auto ml-24 w-[1300px] h-screen scrollbar-thin scrollbar-thumb-blue-600 scrollbar-track-gray-200   p-2 rounded-lg 
